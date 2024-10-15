9c91169edfeded10e561bde91f1d438997f15275
Erik Fatkhutdinov <erik.fatkhutdinov@Erik--MacBook-Pro.local>
2024-10-15 17:30:25 +0200

    renamed usetProfile -> profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Header } from './components/Header'
 import { Navbar } from './components/Navbar'
-import { Profile } from './components/Profile/Profile'
+import { Profile } from './components/Profile'
 
 export default function App() {
     return (
